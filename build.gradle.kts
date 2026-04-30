6f8d2a5e1a5d4149e1ecfa2194d2c68110963b59
Skyjack2033 <skyjack2033@163.com>
2026-04-30 12:07:30 +0800

    fix: narrow JAR exclusions - keep appeng stubs needed at runtime

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,17 +4,8 @@ plugins {
 
 tasks.withType<Jar> {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    // Exclude compatibility stubs that shadow real mod classes at runtime
+    // Only exclude stubs that shadow real mod classes at runtime
     exclude("codechicken/**")
-    exclude("appeng/me/**")
-    exclude("appeng/core/**")
-    exclude("appeng/client/**")
-    exclude("appeng/container/**")
-    exclude("appeng/helpers/**")
-    exclude("appeng/items/**")
-    exclude("appeng/parts/**")
-    exclude("appeng/tile/**")
-    exclude("appeng/util/**")
     exclude("net/minecraft/block/properties/**")
     exclude("net/minecraft/block/state/**")
     exclude("net/minecraft/util/math/**")
@@ -22,7 +13,6 @@ tasks.withType<Jar> {
     exclude("net/minecraftforge/items/**")
     exclude("co/**")
     exclude("com/circulation/**")
-    exclude("com/glodblock/**")
     exclude("com/cleanroommc/**")
     exclude("mezz/**")
 }
