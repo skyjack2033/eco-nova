16891b5b954fc476d2106919d85f05d890ff9389
Skyjack2033 <skyjack2033@163.com>
2026-04-30 11:20:38 +0800

    fix: revert JAR exclusion - stubs needed at runtime

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,4 @@ plugins {
 
 tasks.withType<Jar> {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-    // Exclude AE2 compatibility stubs that shadow real jar classes at runtime
-    exclude("appeng/me/storage/AbstractCellInventory.class")
-    exclude("appeng/me/storage/BasicCellInventoryHandler.class")
 }
